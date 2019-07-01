679cafbe7338813f8399b853ec462af8a5c7a86e
saekiamae <37328610+saekiamae@users.noreply.github.com>
2019-07-01 18:01:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,376 @@
+
+<!doctype html>
+<html xml:lang="en-gb" lang="en-gb" >
+<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+    <base href="https://fight-club.freesite.host/" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="generator" content="Joomla! - Open Source Content Management" />
+	<title>Agario Fight-Club</title>
+	<link href="/?format=feed&amp;type=rss" rel="alternate" type="application/rss+xml" title="RSS 2.0" />
+	<link href="/?format=feed&amp;type=atom" rel="alternate" type="application/atom+xml" title="Atom 1.0" />
+	<link href="/templates/cloudbase3/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+	<link href="/templates/cloudbase3/css-compiled/menu-61b33c78eef42026f28d3e009adf0e3a.css" rel="stylesheet" type="text/css" />
+	<link href="/libraries/gantry/css/grid-responsive.css" rel="stylesheet" type="text/css" />
+	<link href="/templates/cloudbase3/css-compiled/bootstrap.css" rel="stylesheet" type="text/css" />
+	<link href="/templates/cloudbase3/css-compiled/master-8fea0ec006dc30b0683fa48e1ac4bc53.css" rel="stylesheet" type="text/css" />
+	<link href="/templates/cloudbase3/css-compiled/mediaqueries.css" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css" />
+	<style type="text/css">
+#rt-logo {background: url(/images/Fc_logo_kleiner.png) 50% 0 no-repeat !important;}
+#rt-logo {width: 80px;height: 80px;}
+
+h1, h2, h3, h4, h5, h6 { font-family: 'Roboto', 'Helvetica', arial, serif; }
+	</style>
+	<script type="application/json" class="joomla-script-options new">{"csrf.token":"ca1641ff7b70e89905c1a44a468597d0","system.paths":{"root":"","base":""}}</script>
+	<script src="/media/jui/js/jquery.min.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/media/jui/js/jquery-noconflict.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/media/jui/js/jquery-migrate.min.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/media/system/js/caption.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/media/system/js/mootools-core.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/media/system/js/core.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/media/system/js/mootools-more.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script src="/templates/cloudbase3/js/gantry-totop.js" type="text/javascript"></script>
+	<script src="/templates/cloudbase3/js/equalheights.js" type="text/javascript"></script>
+	<script src="/libraries/gantry/js/browser-engines.js" type="text/javascript"></script>
+	<script src="/templates/cloudbase3/js/rokmediaqueries.js" type="text/javascript"></script>
+	<script src="/modules/mod_roknavmenu/themes/default/js/rokmediaqueries.js" type="text/javascript"></script>
+	<script src="/modules/mod_roknavmenu/themes/default/js/sidemenu.js" type="text/javascript"></script>
+	<script src="/media/com_wrapper/js/iframe-height.min.js?09908f8b980ef13792e842abe73f4228" type="text/javascript"></script>
+	<script type="text/javascript">
+jQuery(window).on('load',  function() {
+				new JCaption('img.caption');
+			});
+
+	</script>
+</head>
+<body  class="logo-type-custom body-pattern-none header-style-dark showcase-gradient-direction-horizontal showcase-pattern-none bottom-gradient-direction-horizontal bottom-pattern-default footer-pattern-footer copyright-pattern-footer branding-logo-dark font-family-roboto font-size-is-default menu-type-dropdownmenu layout-mode-responsive col12">
+    	
+	    <div id="rt-menu" class="rt-menu">
+        <div class="rt-container">
+        			<div class="rt-block social-icons">
+    		<ul>
+    											<li>
+			<a id="rt-youtube-btn" target="_blank" href="https://www.youtube.com/channel/UCAy1BAT2X7foQm-LHLsNW1w">
+				<span class="fa fa-youtube"></span>
+			</a>
+			</li>
+												    		</ul>
+		</div>
+		
+	<div class="rt-block menu-block">
+		<div class="gf-menu-device-container responsive-type-panel"></div>
+<ul class="gf-menu l1 " >
+                    <li class="item101 active last" >
+
+            <a class="item" href="/"  >
+
+                                Home                            </a>
+
+
+                    </li>
+                            <li class="item190 parent" >
+
+            <a class="item" href="javascript:void(0);"  >
+
+                                ⚔ℱ𝓒 Members                                <span class="border-fixer"></span>
+                            </a>
+
+
+            
+                                    <div class="dropdown columns-1 " style="width:180px;">
+                                                <div class="column col1"  style="width:180px;">
+                            <ul class="l2">
+                                                                                                                    <li class="item194" >
+
+            <a class="item" href="/members/memberslist.html"  >
+
+                                Members list                            </a>
+
+
+                    </li>
+                                                                                                                                                                <li class="item107" >
+
+            <a class="item" href="/members/member-login.html"  >
+
+                                Member Login                            </a>
+
+
+                    </li>
+                                                                                                        </ul>
+                        </div>
+                                            </div>
+
+                                    </li>
+                            <li class="item191 parent" >
+
+            <a class="item" href="javascript:void(0);"  >
+
+                                ⚔ℱ𝓒 Information                                <span class="border-fixer"></span>
+                            </a>
+
+
+            
+                                    <div class="dropdown columns-1 " style="width:180px;">
+                                                <div class="column col1"  style="width:180px;">
+                            <ul class="l2">
+                                                                                                                    <li class="item192" >
+
+            <a class="item" href="/about/fc-constitution.html"  >
+
+                                Constitution of FC                            </a>
+
+
+                    </li>
+                                                                                                                                                                <li class="item193" >
+
+            <a class="item" href="/about/history.html"  >
+
+                                History of FC                            </a>
+
+
+                    </li>
+                                                                                                        </ul>
+                        </div>
+                                            </div>
+
+                                    </li>
+                            <li class="item195" >
+
+            <a class="item" href="/gallery.html"  >
+
+                                ⚔ℱ𝓒 Gallery                            </a>
+
+
+                    </li>
+                            <li class="item196" >
+
+            <a class="item" href="/contact.html"  >
+
+                                Contact ⚔ℱ𝓒                            </a>
+
+
+                    </li>
+            </ul>		<div class="clear"></div>
+	</div>
+	
+        	<div class="clear"></div>
+        </div>
+    </div>
+    	
+	    <div id="rt-drawer">
+        <div class="rt-container">
+                        <div class="clear"></div>
+        </div>
+    </div>
+        		<div id="rt-showcase">
+		<div class="rt-showcase-pattern">
+			<div class="rt-container">
+				<div class="rt-grid-12 rt-alpha rt-omega">
+               <div class="rt-block clearfix promo">
+           	<div class="module-surround">
+	           		                	<div class="module-content">
+	                		
+
+<div class="custompromo"  >
+	<div class="promo-desc">
+<h1 style="font-weight: 200;">Welcome to <strong>The Agario Fight-Club!</strong></h1>
+<p>&nbsp;</p>
+</div>
+<div class="promo-image"><img src="/images/C_Users_Alessia_AppData_Local_Packages_Microsoft.png" alt="C Users Alessia AppData Local Packages Microsoft" /></div></div>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+		<div id="rt-transition">
+		<div id="rt-mainbody-surround">
+																		<div class="rt-container rt-content">
+		    		          
+<div id="rt-main" class="mb9-sa3">
+                <div class="rt-container">
+                    <div class="rt-grid-9 ">
+                                                						<div class="rt-block">
+	                        <div id="rt-mainbody">
+								<div class="component-content">
+	                            	
+<section class="blog-featured " itemscope itemtype="http://schema.org/Blog">
+	<h1>
+	Agario Fight-Club	</h1>
+
+
+</section>
+
+
+
+								</div>
+	                        </div>
+						</div>
+                                                                        <div id="rt-content-bottom">
+                            <div class="rt-grid-9 rt-alpha rt-omega">
+               <div class="rt-block clearfix ">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">FC advert video</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		<iframe onload="iFrameHeight(this)"	id="blockrandom-135"
+	name=""
+	src="https://www.youtube.com/embed/SfawAU9yz0Q"
+	width="90%"
+	height="430"
+	scrolling="auto"
+	frameborder="1"
+	title="FC advert video"
+	class="wrapper" >
+	No iframes</iframe>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+                        </div>
+                                            </div>
+                                <div class="rt-grid-3 ">
+                <div id="rt-sidebar-a">
+                               <div class="rt-block clearfix nomargintop nopaddingtop">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">Youtube Channels</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		
+
+<div class="customnomargintop nopaddingtop"  >
+	<p>Check out the YouTube Channels of us!</p>
+<p><a href="https://www.youtube.com/channel/UCAy1BAT2X7foQm-LHLsNW1w" target="_blank" rel="noopener noreferrer">Destro.Agar</a></p>
+<p><a href="https://www.youtube.com/channel/UC8M53zwqhP8QHA-e_4-IJZg" target="_blank" rel="noopener noreferrer">Jon Snow</a></p>
+<p><a href="https://www.youtube.com/channel/UC8GPWg0JM2UzOn36JoKPlSw" target="_blank" rel="noopener noreferrer">Central Asia</a></p>
+<p><a href="https://www.youtube.com/channel/UCr0BEU7jfTck7AnchW6ZJ6Q" target="_blank" rel="noopener noreferrer">LD - Agar.io</a></p>
+<p><a href="https://www.youtube.com/channel/UCIYMYvuTnCUplaiZaer4FzQ" target="_blank" rel="noopener noreferrer">Mando</a></p>
+<p><a href="https://www.youtube.com/channel/UCOXRLtlyhxwiEzgxHZ2Q4Gg" target="_blank" rel="noopener noreferrer">Swift</a></p></div>
+	                	</div>
+                	</div>
+           </div>
+	           <div class="rt-block clearfix ">
+           	<div class="module-surround">
+	           		                	<div class="module-content">
+	                		
+
+<div class="custom"  >
+	<div class="feature-img" style="text-align: center;">
+<div><em class="fa fa-user"></em></div>
+</div>
+<h3 style="text-align: center;">Contact us</h3>
+<p style="text-align: center;">Add us in skype and write us.<br />Our Skype ID's:<br />hensch273<br />live:imadabodo33<br />live:florentyna.private<br />allutin112</p></div>
+	                	</div>
+                	</div>
+           </div>
+	
+                </div>
+            </div>
+
+                    <div class="clear"></div>
+                </div>
+            </div>
+		    	</div>
+											</div>
+	</div>
+			<footer id="rt-footer-surround">
+				<div id="rt-footer">
+    		<div class="rt-footer-pattern">
+    			<div class="rt-container">
+    				<div class="rt-grid-3 rt-alpha">
+               <div class="rt-block clearfix hidden-phone">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">Mindset</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		
+
+<div class="customhidden-phone"  >
+	<p>"It is not important that you win. It is important <strong>how</strong> you win. If you win by cowardice, dishonesty and unfairness, <strong>every</strong> win is worthless" - Hendrik</p></div>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+<div class="rt-grid-2">
+               <div class="rt-block clearfix hidden-phone">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">Justice</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		
+
+<div class="customhidden-phone"  >
+	<p>"Justice consists not in being neutral between right and wrong, but in finding out the right and upholding it, wherever found, against the wrong."<br />-&nbsp;Theodore Roosevelt</p></div>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+<div class="rt-grid-2">
+               <div class="rt-block clearfix hidden-phone">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">Fairness</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		
+
+<div class="customhidden-phone"  >
+	<p>"The way you see people is the way you treat them, and the way you treat them is what they become." - Johann Wolfgang von Goethe</p></div>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+<div class="rt-grid-2">
+               <div class="rt-block clearfix ">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">Truth</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		
+
+<div class="custom"  >
+	<p>&ldquo;If absolute truth belongs to anyone in this world, it certainly does not belong to the man or party that claims to possess it.&rdquo; - Albert Camus</p></div>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+<div class="rt-grid-3 rt-omega">
+               <div class="rt-block clearfix ">
+           	<div class="module-surround">
+	           				<div class="module-title">
+	                		<h2 class="title">Hubris</h2>
+			</div>
+	                		                	<div class="module-content">
+	                		
+
+<div class="custom"  >
+	<p>"The&nbsp;attempt&nbsp;to make heaven on&nbsp;earth&nbsp;invariably produces hell." - Sir Karl Raimund Popper</p></div>
+	                	</div>
+                	</div>
+           </div>
+	
+</div>
+    				<div class="clear"></div>
+    			</div>
+    		</div>
+		</div>
+					</footer>
+					<div id="totop">
+					</div>
+				</body>
+</html>
